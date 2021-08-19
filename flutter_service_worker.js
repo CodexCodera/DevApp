2020ccaffbded1dbc482e33ce380501fa02e4a3a
CodexCodera <88572203+CodexCodera@users.noreply.github.com>
2021-08-20 02:32:50 +0300

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,30 +3,30 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "a343d01e8cc8e460d0526fb92f70b5f9",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "37d306708a073cd86096eb7461021345",
+  "DevApp/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"DevApp/assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"DevApp/assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"DevApp/assets/NOTICES": "a343d01e8cc8e460d0526fb92f70b5f9",
+"DevApp/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"DevApp/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"DevApp/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"DevApp/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"DevApp/index.html": "37d306708a073cd86096eb7461021345",
 "/": "37d306708a073cd86096eb7461021345",
-"main.dart.js": "3530f2e3493e07a69a9fe3357656adfe",
-"manifest.json": "31f30ea304d7d1afd4fd4f174e51d72f",
-"version.json": "b46e393bd62212f534f347ddefc2ea13"
+"DevApp/main.dart.js": "3530f2e3493e07a69a9fe3357656adfe",
+"DevApp/manifest.json": "31f30ea304d7d1afd4fd4f174e51d72f",
+"DevApp/version.json": "b46e393bd62212f534f347ddefc2ea13"
 };
 
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
   "/",
-"main.dart.js",
-"index.html",
-"assets/NOTICES",
-"assets/AssetManifest.json",
-"assets/FontManifest.json"];
+"DevApp/main.dart.js",
+"DevApp/index.html",
+"DevApp/assets/NOTICES",
+"DevApp/assets/AssetManifest.json",
+"DevApp/assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
